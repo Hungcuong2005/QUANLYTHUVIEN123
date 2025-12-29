89b11000be152c45eb3b5c40af38d0d7e950e5a9
Pham Hung Cuong <phamhungcuong1112005@gmail.com>
2025-12-30 01:18:36 +0700

    phan 6: trang admin va he thong tu dong gui tin

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
 import authRouter from "./routes/auth.route.js";
 import bookRouter from "./routes/book.route.js";
 import borrowRouter from "./routes/borrow.route.js";
+import userRouter from "./routes/user.route.js";
+import expressFileupload from "express-fileupload"
+import { notifyUsers } from "./services/notifyUsers.js";
+import { removeUnverifiedAccounts } from "./services/removeUnverifiedAccounts.js";
+
 
 
 export const app = express();
@@ -27,12 +32,20 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(
+    expressFileupload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+    })
+);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/book", bookRouter);
 app.use("/api/v1/borrow", borrowRouter);
+app.use("/api/v1/user", userRouter);
 
+notifyUsers(); 
+removeUnverifiedAccounts();
 connectDB();
 
 app.use(errorMiddleware);
