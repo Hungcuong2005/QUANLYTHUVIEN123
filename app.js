31932396d708e5de36c90c11b3ce9f764e7d948d
Pham Hung Cuong <phamhungcuong1112005@gmail.com>
2025-12-29 17:54:04 +0700

    phan 4: bookmanagement

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import { connectDB } from "./database/database.js";
 import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
 import authRouter from "./routes/auth.route.js";
+import bookRouter from "./routes/book.route.js";
 
 
 export const app = express();
@@ -28,6 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/book", bookRouter);
 
 connectDB();
 
