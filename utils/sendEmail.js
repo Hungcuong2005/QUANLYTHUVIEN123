11312948e647497a667a4301cf9a3c95fbd7f8b0
Pham Hung Cuong <phamhungcuong1112005@gmail.com>
2026-01-30 18:27:44 +0700

    phan 22

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,23 @@
-import nodemailer from "nodemailer";
+import nodeMailer from "nodemailer";
 
 export const sendEmail = async ({ email, subject, message }) => {
-  const port = Number(process.env.SMTP_PORT || 465);
+    const transporter = nodeMailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        service: process.env.SMTP_SERVICE,
+        auth: {
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD,
+        },
+    });
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail", // hoặc process.env.SMTP_SERVICE
-    auth: {
-      user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD?.replace(/\s+/g, ""), // loại bỏ khoảng trắng
-    },
-    secure: port === 465,
-    port,
-  });
+    const mailOptions = {
+        from: process.env.SMTP_MAIL,
+        to: email,
+        subject,
+        html: message,
+    };
 
-  const mailOptions = {
-    from: process.env.SMTP_MAIL,
-    to: email,
-    subject,
-    html: message,
-  };
+    await transporter.sendMail(mailOptions);
 
-  await transporter.sendMail(mailOptions);
-};
+}
--- utils/sendVerificationCode.js
+++ utils/sendVerificationCode.js
@@ -1,25 +1,22 @@
 import { generateVerificationOtpEmailTemplate } from "./emailTemplates.js";
 import { sendEmail } from "./sendEmail.js";
 
-export async function sendVerificationCode(verificationCode, email, res) {
-  try {
-    const message = generateVerificationOtpEmailTemplate(verificationCode);
-
-    await sendEmail({
-      email,
-      subject: "Verification Code (Bookworm Library Management System)",
-      message,
+export async function sendVerificationCode (verificationCode, email, res) {
+    try {
+        const message = generateVerificationOtpEmailTemplate(verificationCode);
+        sendEmail({
+            email,
+            subject: "Verification Code (Bookworm Library Management System)",
+            message,
+        });
+        res.status(200).json({
+            success: true,
+            message: "Verification code sent successfully.",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Verification code failed to send.",
     });
-
-    return res.status(200).json({
-      success: true,
-      message: "Verification code sent successfully.",
-    });
-  } catch (error) {
-    console.error("SEND OTP EMAIL ERROR:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Verification code failed to send.",
-    });
-  }
+    }
 }
